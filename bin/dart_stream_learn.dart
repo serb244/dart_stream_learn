d08a0c03c4825121c7916ebd93464b8d3e3cc253
zek24 <zek244@gmail.com>
2023-01-10 18:27:04 +0200

    streamControllerConstructorSync

--- bin/dart_stream_learn.dart
+++ bin/dart_stream_learn.dart
@@ -1,7 +1,131 @@
 import 'dart:async';
 
 void main() {
-  streamControllerClassic();
+  streamControllerConstructorSync();
+}
+
+void streamControllerConstructorSync() {
+  StreamController<int> controller = StreamController(sync: true);
+  StreamSubscription sub;
+  sub = controller.stream.listen((event) {
+    print(event);
+  });
+  print('one');
+  controller.add(1);
+  controller.add(2);
+  controller.add(3);
+  controller.add(4);
+  print('two');
+  controller.add(5);
+  controller.add(6);
+  controller.add(7);
+  Future.delayed(
+    Duration(seconds: 5),
+    () {
+      controller.add(8);
+      controller.close();
+      sub.cancel();
+    },
+  );
+}
+
+void streamControllerConstructor() {
+  StreamController<int> controller = StreamController(
+    onCancel: () => print('onCancel'),
+    onListen: () => print('onListen'),
+    onPause: () => print('onPause'),
+    onResume: () => print('onResume'),
+  );
+  StreamSubscription sub;
+  controller.add(1);
+  controller.add(2);
+  controller.add(3);
+  controller.add(4);
+
+  final stream = controller.stream;
+  sub = stream.listen((event) {
+    print(event);
+  });
+
+  Future.delayed(
+    Duration(seconds: 1),
+    () {
+      controller.onPause!();
+    },
+  );
+  Future.delayed(
+    Duration(seconds: 2),
+    () {
+      controller.onResume!();
+    },
+  );
+  controller.add(5);
+  controller.add(6);
+  controller.add(7);
+  Future.delayed(
+    Duration(seconds: 5),
+    () {
+      controller.add(8);
+      controller.close();
+      sub.cancel();
+    },
+  );
+}
+
+void streamControllerAsyncBroadcast() {
+  StreamController<int> controller = StreamController();
+  StreamSubscription sub;
+  StreamSubscription sub2;
+  controller.add(1);
+  controller.add(2);
+  controller.add(3);
+  controller.add(4);
+
+  final stream = controller.stream.asBroadcastStream();
+  sub = stream.listen((event) {
+    print(event);
+  });
+  sub2 = stream.listen((event) {
+    print(event);
+  });
+  controller.add(5);
+  controller.add(6);
+  controller.add(7);
+  Future.delayed(
+    Duration(seconds: 3),
+    () {
+      controller.add(8);
+      controller.close();
+      sub.cancel();
+    },
+  );
+}
+
+void streamControllerAsync() {
+  StreamController<int> controller = StreamController();
+  StreamSubscription sub;
+  controller.add(1);
+  controller.add(2);
+  controller.add(3);
+  controller.add(4);
+
+  sub = controller.stream.listen((event) async {
+    if (event % 2 == 0) {
+      await Future.delayed(Duration(seconds: 1));
+    }
+    print(event);
+  });
+  controller.add(5);
+  controller.add(6);
+  controller.add(7);
+  Future.delayed(
+    Duration(seconds: 3),
+    () {
+      controller.add(8);
+      controller.close();
+      sub.cancel();
+    },
+  );
 }
 
 void streamControllerClassic() {
@@ -18,7 +142,7 @@ void streamControllerClassic() {
   controller.add(6);
   controller.add(7);
   Future.delayed(
-    Duration(seconds: 5),
+    Duration(seconds: 3),
     () {
       controller.add(8);
       controller.close();
